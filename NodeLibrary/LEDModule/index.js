b810497b950a5293c50054673e8506e7d42a5d0b
Armando Gonzalez <iam@armando.mx>
2017-01-04 19:37:40 -0600

    Added new methods to led implementation

--- NodeLibrary/LEDModule/index.js
+++ NodeLibrary/LEDModule/index.js
@@ -20,9 +20,25 @@ LEDModule.prototype.write = function (ledValue) {
   this.led.write(ledValue);
 };
 
-LEDModule.prototype.led_blink = function () {
+LEDModule.prototype.flash = function () {
   this.led.write(1);
   sleep.usleep(300000);
+  this.led.write(0);
+};
+
+LEDModule.prototype.turnOn = function () {
+  this.led.write(1);
+};
+
+LEDModule.prototype.blink = function () {
+  const self = this;
+  self.interval = setInterval(function () {
+    self.led.flash()
+  }, 1000);
+};
+
+LEDModule.prototype.turnOff = function () {
+  clearInterval(this.interval);
   this.led.write(0);
 };
 
