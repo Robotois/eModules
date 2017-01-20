8dac5b48faa05fb7b5fce43452c553dcc2761886
Armando Gonzalez <iam@armando.mx>
2017-01-20 14:59:16 -0600

    rgb refactor

--- NodeLibrary/RGBModule/index.js
+++ NodeLibrary/RGBModule/index.js
@@ -1,10 +1,11 @@
 var _rgbModule = require('bindings')('RGBModule');
-var hexToRGB = function(hex){
-    const hex = hex.replace('#','');
-    const r = parseInt(hex.substring(0,2), 16);
-    const g = parseInt(hex.substring(2,4), 16);
-    const b = parseInt(hex.substring(4,6), 16);
-    return [r,g,b];;
+var hexToRGB = function(hx){
+    const hex = hx.replace('#','');
+    return [
+      parseInt(hex.substring(0,2), 16),
+      parseInt(hex.substring(2,4), 16),
+      parseInt(hex.substring(4,6), 16)
+    ];
 };
 
 function RGBModule(_add){
