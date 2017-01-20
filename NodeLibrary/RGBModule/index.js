ddb5ec304b33a04c45800aa2bbe06512697d3ccc
Armando Gonzalez <iam@armando.mx>
2017-01-20 13:31:04 -0600

    code higiene

--- NodeLibrary/RGBModule/index.js
+++ NodeLibrary/RGBModule/index.js
@@ -37,55 +37,5 @@ RGBModule.prototype.turnOn = function (ledNumber, hexColor) {
   let rgbColor = hexToRGB(hexColor);
   this.rgb.setRGB(ledNumber.replace('led', ''), rgbColor[0], rgbColor[1], rgbColor[2]);
 };
+
 module.exports = RGBModule;
-
-
-// var sleep = require('sleep');
-//
-// var rgbs = rgbModule();
-//
-// setInterval(function(){
-//   rgbs.BlinkRGB(1,128,0,128); // Purple
-//   // sleep.sleep(1);
-//   // rgbs.ledOff(1); // Purple
-// },5000);
-
-// setInterval(function(){
-//   rgbs.BlinkRGB(2,128,0,128); // Purple
-// },5000);
-//
-// setInterval(function(){
-//   rgbs.BlinkRGB(3,128,0,128); // Purple
-// },3000);
-//
-// rgbs.SetRGB(1,128,0,128); // Purple
-// rgbs.SetRGB(2,0,0,0); // dark turquoise
-// rgbs.SetRGB(3,0,0,0); // Olive
-//
-// sleep.sleep(1);
-// rgbs.ledOff(1);
-// sleep.sleep(1);
-// rgbs.SetRGB(1,128,0,128); // Purple
-//
-// sleep.sleep(1);
-// rgbs.ledOff(1);
-// sleep.sleep(1);
-// rgbs.SetRGB(1,128,0,128); // Purple
-//
-// sleep.sleep(1);
-// rgbs.ledOff(1);
-// sleep.sleep(1);
-// rgbs.SetRGB(1,128,0,128); // Purple
-//
-// sleep.sleep(1);
-// rgbs.ledOff(1);
-// sleep.sleep(1);
-// rgbs.SetRGB(1,128,0,128); // Purple
-//
-// sleep.sleep(1);
-// rgbs.ledOff(1);
-// sleep.sleep(1);
-// rgbs.SetRGB(1,128,0,128); // Purple
-
-// var rgbModule = require('bindings')('RGBModule')
-// module.exports = new rgbModule();
