3033a133aed6951c24832947103f87f0896cb23e
yovany-lg <yovany@gmail.com>
2017-01-20 20:22:09 +0000

    Netbeans config

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=None-Linux-x86
-CND_ARTIFACT_DIR_Debug=dist/Debug/None-Linux-x86
+CND_PLATFORM_Debug=None-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/None-Linux
 CND_ARTIFACT_NAME_Debug=emodules
-CND_ARTIFACT_PATH_Debug=dist/Debug/None-Linux-x86/emodules
-CND_PACKAGE_DIR_Debug=dist/Debug/None-Linux-x86/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/None-Linux/emodules
+CND_PACKAGE_DIR_Debug=dist/Debug/None-Linux/package
 CND_PACKAGE_NAME_Debug=emodules.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/None-Linux-x86/package/emodules.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/None-Linux/package/emodules.tar
 # Release configuration
-CND_PLATFORM_Release=GNU-Linux-x86
-CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
 CND_ARTIFACT_NAME_Release=emodules
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/emodules
-CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/emodules
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
 CND_PACKAGE_NAME_Release=emodules.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/emodules.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/emodules.tar
 #
 # include compiler specific variables
 #
